8a348904e2a7f3db0ccbd34d6dacd44c4841fc9c
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-04 19:54:22 +0100

    Solve exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    for(let i=0; i<arg.length; i++){
+        for(let j=0; j<arr.length; j++){
+            if(arr[j]===arg[i]){
+                arr.splice(j--,1)
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
