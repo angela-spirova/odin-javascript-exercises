e7cb7ee783a488982cfae50cf77dbcdc232c6f3c
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-04 19:10:46 +0100

    Solve exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num){
+    if(num<0){
+        return 'ERROR';
+    }
+    let repeatedString='';
+    while(num){
+        repeatedString+=string;
+        num--;
+    }
+    return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
