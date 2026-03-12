9ca33d4f2b8eb9042b060b022e4f66cc644cc293
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-12 19:51:47 +0100

    Complete exercises

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str = "e") {
+    str = str.toLowerCase();
+    str = str.replaceAll(/[^0-9a-z]/g, "");
+    const halfOfLength = Math.floor(str.length/2);
 
+    for(let i=0; i<halfOfLength; i++){
+        if(str[i]!==str[str.length-1-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
