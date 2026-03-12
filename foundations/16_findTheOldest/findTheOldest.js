3f3d0fdc3099173e179ceddf4bfde71a45eade3c
angel <211508797+angela-spirova@users.noreply.github.com>
2026-03-12 20:40:04 +0100

    Complete exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const currentYear = (new Date()).getFullYear();
+
+    return people.reduce((oldest, person) => {
+        const maxAge = (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+        const age = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+        if(maxAge<age){
+            return person;
+        }
+        return oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
